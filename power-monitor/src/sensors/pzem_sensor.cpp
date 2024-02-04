4f3089af498d55a902aa4ef0ad46b2b030051ade
Emma Balevic <emmabalevic@MacBook-Air-5.local>
2024-02-04 08:59:29 -0500

    added soil moisture sensor code to the pzem_sensor.cpp file

--- power-monitor/src/sensors/pzem_sensor.cpp
+++ power-monitor/src/sensors/pzem_sensor.cpp
@@ -4,6 +4,37 @@
 #include "pzem_sensor.h"
 #include <PZEM004Tv30.h>
 
+const int valvePin1 = 27;
+#define AOUT_PIN_1 26
+
+float value_1;
+const int voltage_1 = 2.3;
+
+void setup() {
+	Serial.begin(115200);
+	pinMode (valvePin1, OUTPUT);
+}
+
+void loop() {
+	value_1 = analogRead(AOUT_PIN_1);
+
+if((value_1/4095*3.3) > voltage_1){
+    //soil is dry and needs to be watered!
+    digitalWrite(valvePin1, HIGH); //turn valve ON
+    Serial.print((value_1/4095*3.3));
+    Serial.print(" - Valve open \n");
+    delay(45000); //leave valve on for 15 seconds
+    digitalWrite(valvePin1, LOW); //turn valve OFF
+  } else {
+    //soil is moist, do not water
+    digitalWrite(valvePin1, LOW);
+    Serial.print((value_1/4095*3.3));
+    Serial.print(" - Valve closed \n");
+    //delay(36000000); //don't check the soil moisture for 10 hours
+  }
+delay(1000);
+}
+
 void ReadPzemSensorData(bool isMQTTConnected){
     // Print out the measured values from each PZEM module
     for(int i = 0; i < Config::NUMBER_OF_PZEMS; i++)
@@ -15,7 +46,7 @@ void ReadPzemSensorData(bool isMQTTConnected){
         float voltage = pzems[i].voltage();
 
 
-        String dataName = "PZEM " + String(i) + " Voltage";
+        String dataName = "SoilMoistureSensor " + String(i) + " Voltage";
         String payloadData = String(voltage);
         String payloadUnits = "(V)";
         LogSensorData(dataName, payloadData, payloadUnits);
