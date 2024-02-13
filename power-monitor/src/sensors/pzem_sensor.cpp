0248eb94395bfc929836c473342e4cfadc153101
Emma Balevic <emmabalevic@MacBook-Air-5.local>
2024-02-12 21:22:49 -0500

    made the pzem_sensor.cpp file a bit more generic so it can be used for all three shelves in the VF

--- power-monitor/src/sensors/pzem_sensor.cpp
+++ power-monitor/src/sensors/pzem_sensor.cpp
@@ -1,41 +1,30 @@
-#include "config.h"
-#include "serial_comm.h"
-#include "mqtt_comm.h"
-#include "pzem_sensor.h"
-#include <PZEM004Tv30.h>
-
-const int valvePin1 = 27;
-#define AOUT_PIN_1 26
-
-float value_1;
-const int voltage_1 = 2.3;
-
 void setup() {
 	Serial.begin(115200);
-	pinMode (valvePin1, OUTPUT);
+	pinMode (valvePin, OUTPUT);
 }
 
 void loop() {
-	value_1 = analogRead(AOUT_PIN_1);
+	sensor_value = analogRead(AOUT_PIN);
 
-if((value_1/4095*3.3) > voltage_1){
+if((sensor_value/4095*3.3) > voltage){
+
     //soil is dry and needs to be watered!
-    digitalWrite(valvePin1, HIGH); //turn valve ON
-    Serial.print((value_1/4095*3.3));
+    digitalWrite(valvePin, HIGH); //turn valve ON
+    Serial.print((sensor_value/4095*3.3));
     Serial.print(" - Valve open \n");
     delay(45000); //leave valve on for 15 seconds
-    digitalWrite(valvePin1, LOW); //turn valve OFF
+    digitalWrite(valvePin, LOW); //turn valve OFF
   } else {
     //soil is moist, do not water
-    digitalWrite(valvePin1, LOW);
-    Serial.print((value_1/4095*3.3));
+    digitalWrite(valvePin, LOW);
+    Serial.print((sensor_value/4095*3.3));
     Serial.print(" - Valve closed \n");
     //delay(36000000); //don't check the soil moisture for 10 hours
   }
 delay(1000);
 }
 
-void ReadPzemSensorData(bool isMQTTConnected){
+void ReadPzemSensorData(bool isMQTTConnected){ ----> i haven't messed with this
     // Print out the measured values from each PZEM module
     for(int i = 0; i < Config::NUMBER_OF_PZEMS; i++)
     {
